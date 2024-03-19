b0f84b79d37e401299dabef9c1062e62d11da66a
n-cupane <92432010+n-cupane@users.noreply.github.com>
2024-03-19 21:08:28 +0100

    Implemented all MovieDao methods

--- src/main/java/com/theotherspace/dao/jdbc/JDBCMovieDao.java
+++ src/main/java/com/theotherspace/dao/jdbc/JDBCMovieDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.theotherspace.dao.MovieDao;
@@ -63,25 +65,109 @@ public class JDBCMovieDao implements MovieDao {
 
 	@Override
 	public List<Movie> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Movie> moviesToReturn = new ArrayList<>();
+		
+		try (Connection c = JDBCDaoFactory.getConnection()) {
+			
+			Statement s = c.createStatement();
+			
+			String query = "select * from movie";
+			
+			ResultSet rs = s.executeQuery(query);
+			
+			while (rs.next()) {
+				
+				Movie m = new Movie();
+				
+				long movieId = rs.getLong("id");
+				long genreId = rs.getLong("genre_id");
+				String title = rs.getString("title");
+				int duration = rs.getInt("duration");
+				String description = rs.getString("description");
+				boolean over18 = rs.getBoolean("for_adults");
+				
+				m.setId(movieId);
+				m.setGenreId(genreId);
+				m.setTitle(title);
+				m.setDuration(duration);
+				m.setDescription(description);
+				m.setOver18(over18);
+				
+				moviesToReturn.add(m);				
+			}
+			
+			return (moviesToReturn.size()>0) ? moviesToReturn : null;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
 	public void update(Movie t) {
-		// TODO Auto-generated method stub
+	
+		try (Connection c = JDBCDaoFactory.getConnection()) {
+			
+			PreparedStatement ps = c.prepareStatement("update movie set genre_id = ?, "
+					+ "title = ?, "
+					+ "duration = ?, "
+					+ "description = ?, "
+					+ "for_adults = ? "
+					+ "where id = ?");
+			
+			ps.setLong(1, t.getGenreId());
+			ps.setString(2, t.getTitle());
+			ps.setInt(3, t.getDuration());
+			ps.setString(4, t.getDescription());
+			ps.setBoolean(5, t.isOver18());
+			ps.setLong(6, t.getId());
+			
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void delete(long id) {
-		// TODO Auto-generated method stub
+		
+		try (Connection c = JDBCDaoFactory.getConnection()) {
+			
+			PreparedStatement ps = c.prepareStatement("delete from movie where id = ?");
+			ps.setLong(1, id);
+			
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void insert(Movie t) {
-		// TODO Auto-generated method stub
+		
+		try (Connection c = JDBCDaoFactory.getConnection()) {
+			
+			PreparedStatement ps = c.prepareStatement("insert into movie("
+					+ "genre_id, title, duration, description, for_adults) "
+					+ "values (?, ?, ?, ?, ?)");
+			
+			ps.setLong(1, t.getGenreId());
+			ps.setString(2, t.getTitle());
+			ps.setInt(3, t.getDuration());
+			ps.setString(4, t.getDescription());
+			ps.setBoolean(5, t.isOver18());
+			
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
