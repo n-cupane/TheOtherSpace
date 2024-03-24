7ef14eace40eccc72bb2ffcf326aec31e449e9f8
n-cupane <92432010+n-cupane@users.noreply.github.com>
2024-03-24 21:21:26 +0100

    Implemented admin control panel to manage users

---
+++ src/main/java/com/theotherspace/controller/UserControlPanelAddServlet.java
@@ -0,0 +1,103 @@
+package com.theotherspace.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+
+import com.theotherspace.model.User;
+import com.theotherspace.utilities.BusinessLogic;
+
+/**
+ * Servlet implementation class UserControlPanelAddServlet
+ */
+public class UserControlPanelAddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String errorMsg;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserControlPanelAddServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("WEB-INF/private-jsp/UserControlPanel-add.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+//		Recupero i dati dal form
+		String firstName, lastName, username, email, password;
+		LocalDate dob;
+		firstName = request.getParameter("add-first-name");
+		lastName = request.getParameter("add-last-name");
+		username = request.getParameter("add-username");
+		email = request.getParameter("add-email");
+		password = request.getParameter("add-password");
+		dob = LocalDate.parse(request.getParameter("add-dob"));
+		
+//		Creo il nuovo utente
+		User u = new User();
+		u.setFirstName(firstName);
+		u.setLastName(lastName);
+		u.setUsername(username);
+		u.setEmail(email);
+		u.setPassword(password);
+		u.setDateOfBirth(dob);
+		
+		if(newUserOk(u)) {
+		    // Se i controlli sul nuovo utente ritornano true, aggiungi l'utente al DB
+		    BusinessLogic.addUser(u);
+		    request.getRequestDispatcher("WEB-INF/private-jsp/UserControlPanel-add.jsp").forward(request, response);
+		    return;
+		}
+		
+//		Se i controlli falliscono mostro il messaggio di errore
+		request.setAttribute("errorMsg", errorMsg);
+		request.getRequestDispatcher("WEB-INF/private-jsp/UserControlPanel-add.jsp").forward(request, response);
+		
+	}
+	
+	public boolean newUserOk(User u) {
+//		Controlli per la creazione e l'aggiunta di un nuovo utente:
+//		- email unica
+//		- username unico
+//		- password di almeno 8 caratteri
+		
+//		Controllo unicità email
+		if (BusinessLogic.findUserByEmail(u.getEmail()) != null) {
+//			imposta messaggio di errore
+			errorMsg = "L'email è già in uso!";
+			return false;
+		}
+		
+//		Controllo unicità username
+		if (BusinessLogic.findUserByUsername(u.getUsername()) != null) {
+//			imposta messaggio di errore
+			errorMsg = "L'username è già in uso!";
+			return false;
+		}
+		
+//		Controllo lunghezza password
+		if (u.getPassword().length() < 8) {
+//			imposta messaggio di errore
+			errorMsg = "La password deve essere di almeno 8 caratteri!";
+			return false;
+		}
+		
+		return true;
+	}
+
+}
---
+++ src/main/java/com/theotherspace/controller/UserControlPanelServlet.java
@@ -0,0 +1,50 @@
+package com.theotherspace.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import com.theotherspace.model.User;
+import com.theotherspace.utilities.BusinessLogic;
+
+/**
+ * Servlet implementation class UserControlPanelServlet
+ */
+public class UserControlPanelServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserControlPanelServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		List<User> users = BusinessLogic.findAllUsers();
+		request.setAttribute("users", users);
+		request.getRequestDispatcher("WEB-INF/private-jsp/UserControlPanel.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int userToRemoveId = Integer.valueOf(request.getParameter("userId"));
+		BusinessLogic.deleteUser(userToRemoveId);
+		List<User> users = BusinessLogic.findAllUsers();
+		request.setAttribute("users", users);
+		request.getRequestDispatcher("WEB-INF/private-jsp/UserControlPanel.jsp").forward(request, response);
+	}
+
+}
